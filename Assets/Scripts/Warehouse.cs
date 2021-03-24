a935d285b2a66e44ed8f1ffcfef004671b3a4a4b
Koksuvi <46352534+MicR12345@users.noreply.github.com>
2021-03-24 12:14:48 +0100

    Test
    test

--- Assets/Scripts/Warehouse.cs
+++ Assets/Scripts/Warehouse.cs
@@ -15,17 +15,17 @@ public class Warehouse : MonoBehaviour
     {
         Builder = gameObject.AddComponent<WarehouseBuilder>();
         Builder.Initialize(this);
-        Builder.CreateFloor(new Vector3(0, 0, 0), new Vector3(40, 0, 40),FloorMaterial);
+        Builder.CreateFloor(new Vector3(0, 0, 0), new Vector3(80, 0, 80),FloorMaterial);
         Builder.CreateWall(new Vector3(0, 0, 0), new Vector3(10, 0, 0),FloorMaterial,5);
         for (int z = 0; z < 10; z++)
         {
             for (int i = 0; i < 10; i++)
             {
-                Builder.CreateShelf(new Vector3(5 + (z * 5), 0, 5 + i * 3), new Vector3(3, 1, 1), ShelfMaterial, 90);
+                Builder.CreateShelf(new Vector3(5 + (z * 5.2f), 0, 5 + i * 3), new Vector3(3, 1, 1), ShelfMaterial, 90);
             }
             for (int i = 0; i < 10; i++)
             {
-                Builder.CreateShelf(new Vector3(6 + (z * 5), 0, 5 + i * 3), new Vector3(3, 1, 1), ShelfMaterial, 90);
+                Builder.CreateShelf(new Vector3(6.2f + (z * 5.2f), 0, 5 + i * 3), new Vector3(3, 1, 1), ShelfMaterial, 90);
             }
         }
 
